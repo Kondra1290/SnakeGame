918a8c70360fd365f0646b08b7f6bb559038815f
Kondra1290 <shura_2002@list.ru>
2023-09-07 21:11:59 +0300

    v1

---
+++ ConsoleApplication3/Fruit.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <time.h>
+
+class Fruit
+{
+private:
+	int x, y;
+
+public:
+
+	int getX() { return x; }
+	void setX(int value) { x = value; };
+	int getY() { return y; }
+	void setY(int value) { y = value; };
+};
+
